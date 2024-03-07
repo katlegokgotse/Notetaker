b1237b068fce3795e0db3f09e95b8bdfa0c0e9fa
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-03-07 22:59:36 +0200

    Note taker

---
+++ src/Task.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+
+export default function Task() {
+  const [tasks, setTasks] = useState(["Eat Breakfast", "Buy Pizza"]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks(t => [...t, newTask]);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  function editTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
+  return (
+    <div className='to-do-list'>
+      <h1>To-Do List</h1>
+
+      <div>
+        <input
+          type='text'
+          placeholder='Enter a task'
+          value={newTask}
+          onChange={handleInputChange} 
+        />
+        <button
+          className='add-button'
+          onClick={addTask} 
+        >
+          Add
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className='text'>{task}</span>
+            <button
+              className='delete-button'
+              onClick={() => deleteTask(index)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
